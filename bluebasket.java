1abd61f365af6c5b369aa2295d2d4053b115a42b
ddavii <150289619+addavi@users.noreply.github.com>
2024-11-21 16:40:51 -0300

    Add files via upload
    21/11/24

---
+++ bluebasket.java
@@ -0,0 +1,40 @@
+package com.example.meepmeep;
+
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class bluebasket {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(690);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(0), 11)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(34, 62, Math.PI + Math.PI/2))
+
+                .strafeToConstantHeading(new Vector2d(9, 35))
+                .waitSeconds(1)
+                .strafeToConstantHeading(new Vector2d(48, 48))
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d( 52, 52), Math.PI + Math.PI/4)
+                .waitSeconds(2)
+                .strafeToLinearHeading(new Vector2d( 58, 46), Math.PI + Math.PI/2)
+                .waitSeconds(1)
+                .strafeToLinearHeading(new Vector2d( 54, 52), Math.PI + Math.PI/4)
+                .waitSeconds(2)
+                .splineTo(new Vector2d(28, 8), Math.PI )
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(1f)
+                .addEntity(myBot)
+                .start();
+    }
+}
